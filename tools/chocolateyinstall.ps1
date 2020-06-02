24e77875b62524dc0ede4dd05bd28f733e881a53
Jeff Boccuzzi <jeff.boccuzzi@enowsoftware.com>
2020-06-02 16:54:21 -0700

    Create initial package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$checksum = 'ad3f594aaf532f42aae7760787b749995eb3f5769c9044ce088cf502ed60e337'
+$url = 'https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi'
+
+$softwareName = 'Microsoft Azure AD Connect'
+
+$packageArgs = @{
+  packageName   = 'azure-ad-connect'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'msi'
+  url           = $url
+  silentArgs    = '/quiet /norestart'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName  = $softwareName
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
